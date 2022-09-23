4a238f150e60e462bdcf776ffdc9c1b57de97bc8
Daniel Nguyen <dnguyen23@hanoverstudents.org>
2022-09-23 17:49:56 -0400

    mult and div progress

---
+++ target/debug/deps/Error_Calc.d
@@ -0,0 +1,5 @@
+C:\Users\danie\Rust\Projects\SigFigs-Error-Calculator\target\debug\deps\Error_Calc.exe: src\main.rs
+
+C:\Users\danie\Rust\Projects\SigFigs-Error-Calculator\target\debug\deps\Error_Calc.d: src\main.rs
+
+src\main.rs:
